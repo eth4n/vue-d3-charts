ecc06b93ec973495d59bb9c1723ab99cb62cf79f
Christoph Lange <christoph.lange@daimler.com>
2021-01-14 13:10:21 +0100

    Exported chart base class and linechart class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable import/prefer-default-export */
+import d3chart from "./d3.chart";
+import d3linechart from './linechart/d3.linechart';
+
 export { default as D3BarChart } from './barchart/d3.barchart.vue';
 export { default as D3LineChart } from './linechart/d3.linechart.vue';
 export { default as D3PieChart } from './piechart/d3.piechart.vue';
@@ -6,3 +9,6 @@ export { default as D3SlopeChart } from './slopechart/d3.slopechart.vue';
 export { default as D3Sunburst } from './sunburst/d3.sunburst.vue';
 export { default as D3WordsCloud } from './wordscloud/d3.wordscloud.vue';
 export { default as D3SlicesChart } from './sliceschart/d3.sliceschart.vue';
+export { default as D3Chart } from './d3.chart.vue';
+
+export { d3chart as D3ChartClass, d3linechart as D3LineChartClass }
